8ee3cd9ae36f10c25eb3f344b5d5cb64e4d1b8ad
Gerardo Andres Gutierrez Rodriguez <geangu@gmail.com>
2020-07-23 20:22:23 -0300

    remove unecesary ctx variable

--- helpers.go
+++ helpers.go
@@ -13,14 +13,13 @@ import (
 // RedisHelper struct
 type RedisHelper struct{}
 
-func (r *RedisHelper) getClient() (context.Context, *redis.Client) {
-	ctx := context.Background()
+func (r *RedisHelper) getClient() *redis.Client {
 	client := redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
-	return ctx, client
+	return client
 }
 
 // SaveLocationRedis method
@@ -30,8 +29,8 @@ func (r *RedisHelper) SaveLocationRedis(l *Location) {
 		fmt.Println(err)
 	}
 
-	ctx, client := r.getClient()
-	err = client.Set(ctx, l.Key, string(content), 0).Err()
+	client := r.getClient()
+	err = client.Set(l.Key, string(content), 0).Err()
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -40,8 +39,8 @@ func (r *RedisHelper) SaveLocationRedis(l *Location) {
 // GetLocation by key
 func (r *RedisHelper) GetLocation(provider string) Location {
 	var l Location
-	ctx, client := r.getClient()
-	val, err := client.Get(ctx, provider).Result()
+	client := r.getClient()
+	val, err := client.Get(provider).Result()
 	if err != nil {
 		fmt.Println(err.Error())
 		return l
